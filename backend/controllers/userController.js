14cc209ab29e6a78454f52336f758f22e189065b
shibzz <monsterbegin@gmail.com>
2026-02-14 17:19:22 +0530

    res changed in getAllUsers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import { generateToken } from "../utils/generateToken.js";
 export const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({}).select("-password");
-    return res.status(200).json({ count: users.length, data: users });
+    return res.status(200).json(users);
   } catch (error) {
     return res.status(500).json({
       message: "Server Error: Could not fetch users",
