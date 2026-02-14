adbfed8ef14d291a5f4f115b27b62f8278524c08
shibzz <monsterbegin@gmail.com>
2026-02-14 16:31:33 +0530

    feat(users): Add getAllUsers route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getAllUsers,
   loginUser,
   logoutUser,
   registerUser,
@@ -10,6 +11,7 @@ import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/", getAllUsers);
 router.post("/register", registerUser);
 router.post("/login", limiter, loginUser);
 router.post("/logout", logoutUser);
